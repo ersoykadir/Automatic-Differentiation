c94e9a48be8a5ad1a722c6f6fa3b0fd5713d8c13
kadir <eersooy61@gmail.com>
2021-06-16 17:06:33 +0300

    cleanupv2

--- autodiff.rkt
+++ autodiff.rkt
@@ -17,11 +17,11 @@
 (define mse (lambda (x y) (mul (sub x y) (sub x y))))
 (define (get-value num-list)
     (if (list? num-list) 
-    (map num-value num-list) (num-value num-list)
+        (map num-value num-list) (num-value num-list)
     ))
 (define (get-grad num-list)
     (if (list? num-list) 
-    (map num-grad num-list) (num-grad num-list)
+        (map num-grad num-list) (num-grad num-list)
     ))
 (define (sub num1 num2)
     (num (- (num-value num1) (num-value num2)) (- (num-grad num1) (num-grad num2)))
@@ -30,13 +30,13 @@
     (let ((v-list (get-value args)) (g-list (get-grad args)))
     (num (apply + v-list) (apply + g-list))
     ))
-(define (listcheck ls)
+(define (listcheck ls);if list return, else make list and return    (kind of typecasting to list)
     (if (list? ls)
         ls
         (list ls)
     )
 )
-(define (grad-mul args len)
+(define (grad-mul args len); rotates the list and calculates the gradient according to the head of the list recursively
     (if (eqv? len 0)
         0
         (+ (foldl * (num-grad(car args)) (listcheck (get-value(cdr args))))       
@@ -51,10 +51,14 @@
         (+ 1 (len (cdr list)))
     )
 )
-(define (mul . args);num 5 1 num 2 1
-    (let ((v-list (get-value args)))
-        (num (apply * v-list) 
-            (+ (foldl * (num-grad(car args)) (listcheck (get-value(cdr args))))      
+(define (mul . args);
+    (let ((v-list (get-value args)));get values
+        (num (apply * v-list) ; making a num struct out of multiplication of nums and multiplication of grads(according to chain rule) 
+            ;gradient calc 
+                ;;get the first num's gradient and multiply it with the values of the rest of the nums
+            (+ (foldl * (num-grad(car args)) (listcheck (get-value(cdr args))))    
+                ;then rotate left the args list by taking its first element and putting it to the end of the list
+                ;do the same gradient calc, stop when the args list returns to its original self after rotations
                 (grad-mul 
                     (append (listcheck (cdr args)) (listcheck (car args))) 
                     (- (len args) 1)
@@ -63,34 +67,21 @@
         )
     )
 )
-(define (chash name val var)
+(define (chash name val var); create num structs, makes gradient 1 if name is equal to var 
     (if (eqv? var name)
         (num val 1.0)
         (num val 0.0)
     ))
-(define (varlist names var)
-    (if (null? names)
+(define (varlist lis var);helperfunction--> creates a list by repeating the given variable for length of the given list times
+    (if (null? lis)
         '()
-        ;(foldl list var (varlist (cdr names) var)) 
-        (append (list var) (varlist (cdr names) var))
-        ;(list var (varlist (cdr names) var))
-    ))
-(define (comb-two-list lis1 lis2)
-    (if (null? lis1)
-        '()
-        (append (list (car lis1) (car lis2)) (comb-two-list (cdr lis1) (cdr lis2)) )
+        (append (list var) (varlist (cdr lis) var))
     )
 )
-(define (create-hash names values var)
+(define (create-hash names values var);; creates num structs from given values, hashes the names to those num structs
     (let ((ls (map chash names values (varlist names var))))
         (make-hash (map cons names ls))
     )        
-)
-(define (get-val sym hash)
-    (if (number? sym) 
-        (num sym 0.0)
-        (hash-ref hash sym)
-    )
 )
 (define (parse hash expr)
     (cond 
@@ -112,15 +103,15 @@
 )(define (partial-grad names values vars expr)
     (get-partial-grad names values vars expr names)
 )
-(define (get-partial-grad names values vars expr head)
+(define (get-partial-grad names values vars expr tempNames);; helper func for calling partial-grad recursively, shrinking the temporary names list
     (if (null? vars)
-        (if (null? head) 
+        (if (null? tempNames) ; since vars is empty the rest of the grads will be zero 
             '()
-            (append '(0.0) (get-partial-grad names values vars expr (cdr head)) )
+            (varlist tempNames 0.0)
         )
-        (if (eqv? (car head) (car vars)) 
-            (append (listcheck (grad names values (car vars) expr)) (listcheck (get-partial-grad names values (cdr vars) expr (cdr head) )))   
-            (append '(0.0) (listcheck (get-partial-grad names values vars expr (cdr head) )) )
+        (if (eqv? (car tempNames) (car vars)) ;if the head of names list is in vars get its grad, if not put 0 and calculate the rest
+            (append (listcheck (grad names values (car vars) expr)) (listcheck (get-partial-grad names values (cdr vars) expr (cdr tempNames) )))   
+            (append '(0.0) (listcheck (get-partial-grad names values vars expr (cdr tempNames) )) )
         )
     )
 )
